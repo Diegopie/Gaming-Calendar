0aef1366b2b0dfd0ca51d5c74c27bb3dfee45c52
Jordan Kirby <jrock255@hotmail.com>
2020-08-29 12:06:48 -0600

    auto play

---
+++ scripts.js
@@ -0,0 +1,10 @@
+ bulmaCarousel.attach('#slider', {
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    navigationKeys: true,
+    navigation: true,
+ });
+
