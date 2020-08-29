a70cab4e67ec8720ed57d8b895bea892acc87f28
Jordan Kirby <jrock255@hotmail.com>
2020-08-29 13:04:18 -0600

    moved the carusel code over to index.js

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,10 @@
 // JS Code for Carousel
-   bulmaCarousel.attach('#slider', {
-        slidesToScroll: 1,
-        slidesToShow: 1,
-        infinite: true,
-    });
-    
+bulmaCarousel.attach('#slider', {
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    navigationKeys: true,
+    navigation: true,
+});
