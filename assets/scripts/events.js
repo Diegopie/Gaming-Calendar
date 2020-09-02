051afd1b3014248e52e941f1603a903650798886
Diego Hernandez <ishtaracademy@outlook.com>
2020-09-01 20:27:41 -0600

    removed placeholder content in game container; dom now updates with relevant data based on user search

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -28,7 +28,7 @@
         $.ajax({
             url: url,
             method: "GET"
-        }) .then (function current(activeGame) {
+        }) .then (function (activeGame) {
             console.log(url)
                 console.log(activeGame); //this returns all games with selectedGame in their title
                 // console.log(activeGame.results);
@@ -37,9 +37,55 @@
             gameData.image = gameInfo.background_image;
             gameData.platforms = gameInfo.platforms;
             gameData.store = gameInfo.stores;
-            console.log(gameData);
+            gameData.genres = gameInfo.genres;
+            gameData.released = gameInfo.released
+
+            // console.log(gameData);
             
             // This function will be used once we add logic for updating the DOM
-            // renderGame()
+            renderGame()
         });
+    }
+
+
+    function renderGame() {
+        let title = $('<h2>').text(gameData.title).addClass("title is-4 game-title")
+        let img = $('<img>').attr('src', gameData.image).addClass("whatever")
+        let platString = ""
+        let storeString = ""
+        let genresString = ""
+        console.dir(gameData);
+        // Add all the game platforms to a string
+        console.log(gameData.platforms.length);
+        for (i = 0; i < gameData.platforms.length; i++) {
+            // console.log(gameData.platforms[i].platform.name);
+            platString += gameData.platforms[i].platform.name + ", ";
+            // console.log(platString);
+        }
+        // Add all the game stores to a string
+        console.log(gameData.store.length);
+        for (i = 0; i < gameData.store.length; i++) {
+            // console.log(gameData.store[i].store.name);
+            storeString += gameData.store[i].store.name + ", ";
+            // console.log(storeString);
+        }
+         // Add all the game genres to a string
+         console.log(gameData.genres.length);
+         for (i = 0; i < gameData.genres.length; i++) {
+             console.log(gameData.genres[i].name);
+             genresString += gameData.genres[i].name + ", ";
+             console.log(genresString);
+         }
+
+
+        
+        // $('#usethis').append(title, img, platforms)
+        $('#game-titl').append(title)
+        $('#game-img').append(img)
+        $('#platforms').text(platString)
+        $('#store').text(storeString)
+        $('#genres').text(genresString)
+        $('#date').text(gameData.released)
+
+
     }
