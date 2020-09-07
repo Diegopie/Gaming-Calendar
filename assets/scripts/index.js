27dad9a1b5b36ff3610bdaf685d839e7f1f24523
Diego Hernandez <ishtaracademy@outlook.com>
2020-09-07 11:36:42 -0600

    commented out the console logs, added a line of HTML to events so that the create button moves you down the page to QR container

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -19,63 +19,63 @@ $.ajax({
 }).then(function (response) {
 
     i = Math.floor(Math.random() * 20);
-    console.log(response)
+    // console.log(response);
     //function(response)
     $('#randomPic1').html('<img src=' + response.results[i].background_image + ' />').attr('alt', response.results[i].name).attr('value', response.results[i].name);
     $('.item__title1').html(response.results[i].name);
-})
+});
 
 $.ajax({
     url: 'https://api.rawg.io/api/games?dates=2010-01-01,2020-09-01&ordering=-added',
     method: 'GET',
 }).then(function (response) {
     i = Math.floor(Math.random() * 20);
-    console.log(response)
+    // console.log(response);
     //function(response)
     $('#randomPic2').html('<img src=' + response.results[i].background_image + ' />').attr('alt', response.results[i].name).attr('value', response.results[i].name);
     $('.item__title2').html(response.results[i].name);
-})
+});
 
 $.ajax({
     url: 'https://api.rawg.io/api/games?dates=2010-01-01,2020-09-01&ordering=-added',
     method: 'GET',
 }).then(function (response) {
     i = Math.floor(Math.random() * 20);
-    console.log(response)
+    // console.log(response);
     //function(response)
     $('#randomPic3').html('<img src=' + response.results[i].background_image + ' />').attr('alt', response.results[i].name).attr('value', response.results[i].name);
     $('.item__title3').html(response.results[i].name);
-})
+});
 
 $.ajax({
     url: 'https://api.rawg.io/api/games?dates=2010-01-01,2020-09-01&ordering=-added',
     method: 'GET',
 }).then(function (response) {
     i = Math.floor(Math.random() * 20);
-    console.log(response)
+    // console.log(response);
     //function(response)
     $('#randomPic4').html('<img src=' + response.results[i].background_image + ' />').attr('alt', response.results[i].name).attr('value', response.results[i].name);
     $('.item__title4').html(response.results[i].name);
-})
+});
 
 $.ajax({
     url: 'https://api.rawg.io/api/games?dates=2010-01-01,2020-09-01&ordering=-added',
     method: 'GET',
 }).then(function (response) {
     i = Math.floor(Math.random() * 20);
-    console.log(response)
+    // console.log(response);
     //function(response)
     $('#randomPic5').html('<img src=' + response.results[i].background_image + ' />').attr('alt', response.results[i].name).attr('value', response.results[i].name);
     $('.item__title5').html(response.results[i].name);
-})
+});
 
 // * Listener To Send User to Events and Load the Game They Clicked
 $("#slider").click(function(){
     // Get value of what they clicked
-    let userGame = $(event.target).parent()[0].attributes[3].value
-            console.dir(userGame);
+    let userGame = $(event.target).parent()[0].attributes[3].value;
+            // console.dir(userGame);
     // Store value in local storage
-    localStorage.setItem('passGame', userGame)
+    localStorage.setItem('passGame', userGame);
     // Send user to events.html
-    window.location.href = "events.html"
-})
+    window.location.href = "events.html";
+});
